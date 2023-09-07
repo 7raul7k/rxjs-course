e677b0dd80b5a1f53d248b3f6559c779e0f08a11
7raul7k <balanescuraul@gmail.com>
2023-09-07 17:23:06 +0300

    add observable-create

---
+++ observable_create.js
@@ -0,0 +1,25 @@
+let {Observable} = require = require('rxjs/Observable');
+
+function getData(){
+
+    var beers = [
+    {name: "Stella", country: "Belgium", price: 9.50},
+    {name: "Sam Adams", country: "USA", price: 8.50},
+    {name: "Bud Light", country: "USA", price: 6.50},
+    {name: "Brooklyn Lager", country: "USA", price: 8.00},
+    {name: "Sapporo", country: "Japan", price: 7.50}
+    ];
+
+    return Observable.create(
+        observer =>{
+        beers.forEach(beer => observer.next(beer));
+        observer.complete();
+        });
+    }
+
+    getData()
+        .subscribe(
+            beer => console.log("Subscriber got " + beer.name),
+            err => console.error(err),
+            () => console.log('Streaming is over')
+);
